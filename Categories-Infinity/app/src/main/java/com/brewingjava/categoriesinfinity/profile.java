41b469b9801e6d675b49ef6bbacee11689c63a77
Kaitlynst1002 <kaitlynst1002@gmail.com>
2020-11-03 17:31:40 -0500

    "Update Login"

--- Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/profile.java
+++ Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/profile.java
@@ -1,5 +1,6 @@
 package com.brewingjava.categoriesinfinity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,16 +17,23 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+    public void signUpToMain(View view){
+        //send to profile
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+//    Toolbar toolbar = findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
+//    FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener(){
+//        @Override
+//        public void onClick(View view) {
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show();
+//        }
+//    });
 }
