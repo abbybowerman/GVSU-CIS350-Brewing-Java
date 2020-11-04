abb9265140dc040c57b572c4f9cefd9a8c5d0bb5
Kaitlynst1002 <kaitlynst1002@gmail.com>
2020-11-04 13:38:11 -0500

    "Update Main"

--- Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/MainActivity.java
+++ Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/MainActivity.java
@@ -18,6 +18,5 @@ public class MainActivity extends AppCompatActivity {
     public void profilePage(View view) {
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
-
     }
 }
