58dcd3d601f2e6d76355392f90e8fef5ad44c744
Abby Bowerman <abbybowerman@outlook.com>
2020-12-11 11:00:04 -0500

    edited constraints on profile page

--- Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/ProfileActivity.java
+++ Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.brewingjava.categoriesinfinity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        final Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        final Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
