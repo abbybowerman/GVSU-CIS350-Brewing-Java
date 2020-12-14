cbd09b0667a68586e3b4eaaeed33693d43587dca
Abby Bowerman <abbybowerman@outlook.com>
2020-12-14 16:03:03 -0500

    added key to private java file, regenerated key

--- Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/GameActivity.java
+++ Categories-Infinity/app/src/main/java/com/brewingjava/categoriesinfinity/GameActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        Secrets s = new Secrets();
+
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), s.apikey);
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(this);
     }
 }
